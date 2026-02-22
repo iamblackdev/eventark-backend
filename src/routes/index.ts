dbca51949e5b186442f6ebc379fcc2a44cc792f4
iamblackdev <wahabhabib23@gmail.com>
2026-02-22 15:41:31 +0000

    update env value

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,7 +10,7 @@ import session from 'express-session';
 import passport from 'passport';
 import MongoStore from 'connect-mongo';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENVR === 'production';
 
 export default function (app: Express) {
 	app.use(cors());
